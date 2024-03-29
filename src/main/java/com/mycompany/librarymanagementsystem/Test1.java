277dde00d1e9ab24b3f1132585beff0daa34c03c
Sokmeak <saroussokmeak721@gmail.com>
2024-03-29 23:15:35 +0700

    add file Test to repo

---
+++ src/main/java/com/mycompany/librarymanagementsystem/Test1.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.librarymanagementsystem;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Java Swing");
+        System.out.println("How are you doing today?");
+        
+        for(int i = 0; i<10;i++){
+            System.out.println(i);
+        }
+        
+        
+       
+                
+    }
+    
+}
